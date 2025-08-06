2335d0b0e67c0e112b416e8f2405c3daed1227b2
Harvey Cao <harvey.cao.yy@gmail.com>
2025-08-06 21:59:06 +0000

    add device name to log

--- models/view_tuya_wifi_socket_add_ele_log.sql
+++ models/view_tuya_wifi_socket_add_ele_log.sql
@@ -1,9 +1,12 @@
 SELECT 
   event_time,
-  device_id,
+  lg.device_id,
+  d.name AS device_name,
   add_ele/1000 AS add_ele,
   FORMAT_TIMESTAMP("%c",TIMESTAMP_MILLIS(event_time)) AS datetime_utc,
   FORMAT_TIMESTAMP("%c",TIMESTAMP_MILLIS(event_time),'Pacific/Auckland') AS datetime_nz,
   EXTRACT(DATE FROM TIMESTAMP_MILLIS(event_time) AT TIME ZONE 'Pacific/Auckland') AS date,
   EXTRACT(HOUR FROM TIMESTAMP_MILLIS(event_time) AT TIME ZONE 'Pacific/Auckland') AS hour
-FROM {{ source('tuya', 'tuya_wifi_socket_add_ele_log')}}
+FROM {{ source('tuya', 'tuya_wifi_socket_add_ele_log')}} lg
+LEFT JOIN {{ref('devices')}} d
+ON lg.device_id=d.device_id
